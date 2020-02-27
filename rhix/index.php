aab45037019a606f70917715c9fb5f2241af3333
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-02-27 11:17:27 +0800

    Update index.php

--- rhix/index.php
+++ rhix/index.php
@@ -1,6 +1,5 @@
 <?php
   $load = (isset($_GET['page']) && $_GET['page'] != '') ? $_GET['page'] : '';
-  $action = (isset($_GET['action']) && $_GET['action'] != '') ? $_GET['action'] : '';
 ?>
 <!DOCTYPE html>
 <html>
