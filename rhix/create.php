451159e2a96373bbe2e527f988eeb7c34e6a6222
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-03-03 11:01:17 +0800

    Update create.php

--- rhix/create.php
+++ rhix/create.php
@@ -1,3 +1,10 @@
+ <?php 
+  $json1 = file_get_contents('http://rdapi.herokuapp.com/category/read.php);
+  $data1 = json_decode($json1,true);
+  $details1 = array('records' => $data1);
+  $result1 = $details1['records'];
+ ?>
+
 <div class="container">
   <h1> ADD PRODUCT</h1>
   <form action="createProd.php" method="POST">
@@ -8,7 +15,15 @@
     <p>Price:</p>
     <input type="text" name="price" placeholder="Price"/><br>
     <p>Category:</p>
-    <input type="text" name="category" placeholder="Category"/><br>
+    <select>
+      <?php
+        foreach($list as $result1) { 
+      ?>
+      <option value="<?php echo $result1['category_id']?>"><?php echo $result1['category_name']?></option>
+      <?php 
+        } 
+      ?>
+    </select>
     <input type="submit" name="submit" value="Add"/>
   </form>
 </div>
