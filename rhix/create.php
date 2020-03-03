59a28e65deb527e920b04f0577fdbd6646c5fa9b
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-03-03 11:20:00 +0800

    Update create.php

--- rhix/create.php
+++ rhix/create.php
@@ -14,7 +14,7 @@
     <p>Price:</p>
     <input type="text" name="price" placeholder="Price"/><br>
     <p>Category:</p>
-    <select type="text" name="category">
+    <select class="custom-select" "type="text" name="category">
 					<option value="" selected> ---Category--- </option>
 				<?php
 					foreach($category as $catSelect)
