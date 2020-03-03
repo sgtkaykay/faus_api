f4fc1bc7d9070fffc14af509161b662fca7ca5b5
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-03-03 10:57:02 +0800

    Update update.php

--- rhix/update.php
+++ rhix/update.php
@@ -4,6 +4,11 @@
   $data = json_decode($json,true);
   $details = array('records' => $data);
   $result = $details['records'];
+
+  $json1 = file_get_contents('http://rdapi.herokuapp.com/category/read.php);
+  $data1 = json_decode($json,true);
+  $details1 = array('records' => $data);
+  $result1 = $details1['records'];
 ?>
 <div class="container">
   <h1> ADD PRODUCT HERE</h1>
@@ -15,7 +20,16 @@
     <p>Price:</p>
     <input type="text" name="price" value="<?php echo $result['price']; ?>"/>
     <p>Category:</p>
-    <input type="text" name="category" value="<?php echo $result['category_id'];?>"/></br>
+    <select>
+      <option value="<?php echo $result['category_id']?>"><?php echo $result['category_name']?></option>
+      <?php
+        foreach($list as $result1) { 
+      ?>
+      <option value="<?php echo $result['category_id']?>"><?php echo $result['category_name']?></option>
+      <?php 
+        } 
+      ?>
+    </select>
     <input type="submit" name="submit" value="Update"/>
   </form>
 </div>
